7b5be40c368489eb8e92c50af0db8efa1a8d7fa7
alxkorn <akorneyev10@yandex.ru>
2019-11-27 22:57:39 +0300

    Reworked create_answer_scpec to satisfy constraint: "Onlu authenticated user can create questions and answers"

--- spec/features/answer/create_answer_spec.rb
+++ spec/features/answer/create_answer_spec.rb
@@ -2,35 +2,37 @@
 
 require 'rails_helper'
 
-feature "User can answer question while being on question's page", "
+feature "Authenticated User can answer question while being on question's page", "
 In order to help someone to resolve the problem
-As a user
+As an authenticated user
 I'd like to be able to answer question
 " do
-  given(:user) { create(:user) }
   given(:question) { create(:question) }
+  describe 'Authenticated user' do
+    given(:user) { create(:user) }
 
-  background do
-    # sign_in(user)
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'answers the question' do
+      fill_in 'Body', with: 'Answer text'
+
+      click_on 'Submit answer'
+      expect(page).to have_content 'Answer text'
+    end
+
+    scenario 'tries to answer the question with errors' do
+      click_on 'Submit answer'
+      expect(page).to have_content "Body can't be blank"
+    end
+  end
+
+  scenario 'Unauthenticated user tries to answer the question' do
     visit question_path(question)
-  end
-
-  scenario 'answers the question' do
-    fill_in 'Body', with: 'Answer text'
-
     click_on 'Submit answer'
-    expect(page).to have_content 'Answer text'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
   end
-
-  scenario 'tries to answer the question with errors' do
-    click_on 'Submit answer'
-    expect(page).to have_content "Body can't be blank"
-  end
-
-  # scenario 'Unauthenticated user tries to answer the question' do
-  #   # visit question_path(question)
-  #   click_on 'Submit answer'
-
-  #   expect(page).to have_content ''
-  # end
 end
