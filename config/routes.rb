1dc2117ef62b29718f23284efaf32226451a26ae
alxkorn <akorneyev10@yandex.ru>
2019-11-28 15:59:30 +0300

    Fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :questions do
-    resources :answers, shallow: true, except: %i[index]
+    resources :answers, shallow: true, only: %i[destroy create]
   end
 
   root to: 'questions#index'
