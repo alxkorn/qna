5cdf01b6900138a62ce4427865849b0fbb292cf1
alxkorn <akorneyev10@yandex.ru>
2019-11-30 23:44:37 +0300

    Completed answers/edit_spec

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 feature 'User can edit his answer', "
@@ -5,10 +7,10 @@ feature 'User can edit his answer', "
   As an autjor of answer
   I'd like to be able to edit my answer
 " do
-
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question, user: user) }
+  given(:answer_not_owned) { create(:answer) }
 
   scenario 'Unanuthenticated user can not edit answer' do
     visit question_path(question)
@@ -17,12 +19,13 @@ feature 'User can edit his answer', "
   end
 
   describe 'Authenticated user' do
+    background { sign_in(user) }
+
     scenario 'edits his answer', js: true do
-      sign_in(user)
       visit question_path(question)
-      click_on 'Edit'
 
       within '.answers_list' do
+        click_on 'Edit'
         fill_in 'Body', with: 'edited answer'
         click_on 'Save'
 
@@ -32,7 +35,24 @@ feature 'User can edit his answer', "
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's answer"
+    scenario 'edits his answer with errors', js: true do
+      visit question_path(question)
+
+      within '.answers_list' do
+        click_on 'Edit'
+        fill_in 'Body', with: ''
+        click_on 'Save'
+
+        expect(page).to have_content "Body can't be blank"
+      end
+    end
+
+    scenario "tries to edit other user's answer", js: true do
+      visit question_path(answer_not_owned.question)
+
+      within '.answers_list' do
+        expect(page).to_not have_link 'Edit'
+      end
+    end
   end
 end
