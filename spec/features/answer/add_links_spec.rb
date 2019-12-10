3e820bbae9daf6bda553a3fb8dd12e1555f1e4b7
alxkorn <akorneyev10@yandex.ru>
2019-12-10 14:23:10 +0300

    Implemented feature 'User can add multiple links when creationg answer'

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -8,6 +8,7 @@ feature 'User can add links to answer', "
   given(:user) { create(:user) }
   given(:question) { create(:question) }
   given(:gist_url) { 'https://gist.github.com/alxkorn/26076b45ec43c8e4ea10bb5d32d1f7fc' }
+  given(:google) { 'google.com' }
 
   scenario 'User adds link when give answer', js: true do
     sign_in(user)
@@ -15,14 +16,20 @@ feature 'User can add links to answer', "
 
     fill_in 'Body', with: 'Answer text'
 
-    fill_in 'Link name', with:'My gist' 
+    fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
+    click_on 'Add link'
+    within page.all(:css, '.nested-fields')[1] do
+      fill_in 'Link name', with: 'Google'
+      fill_in 'Url', with: google
+    end
 
     click_on 'Submit answer'
 
     within '.answers_list' do
       expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: google
     end
   end
 
