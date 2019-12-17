273a3686b1410de21429899f7f61cc7e7c4432ba
alxkorn <akorneyev10@yandex.ru>
2019-12-17 13:22:13 +0300

    Implemented feature answer appears on another user's page

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -8,9 +8,9 @@ As an authenticated user
 I'd like to be able to answer question
 " do
   given(:question) { create(:question) }
+  given(:user) { create(:user) }
 
   describe 'Authenticated user' do
-    given(:user) { create(:user) }
 
     background do
       sign_in(user)
@@ -18,14 +18,18 @@ I'd like to be able to answer question
     end
 
     scenario 'answers the question', js: true do
-      fill_in 'Body', with: 'Answer text'
+      within '.new-answer-form' do
+        fill_in 'Body', with: 'Answer text'
+      end
 
       click_on 'Submit answer'
       expect(page).to have_content 'Answer text'
     end
 
     scenario 'creates answer with attached files', js: true do
-      fill_in 'Body', with: 'Answer text'
+      within '.new-answer-form' do
+        fill_in 'Body', with: 'Answer text'
+      end
 
       attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on "Submit answer"
@@ -45,5 +49,31 @@ I'd like to be able to answer question
     click_on 'Submit answer'
 
     expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
+
+  context 'multiple sessions' do
+    scenario "answer appears on another user's page", js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within '.new-answer-form' do
+          fill_in 'Body', with: 'Answer text'
+        end
+
+        click_on 'Submit answer'
+        expect(page).to have_content 'Answer text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'Answer text'
+      end
+    end
   end
 end
