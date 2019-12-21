326501b0895ace03c79b329ebc36449a41691cd3
alxkorn <akorneyev10@yandex.ru>
2019-12-21 21:24:37 +0300

    Added shared examples for API authorizable

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -9,17 +9,9 @@ describe 'Questions API', type: :request do
   end
 
   describe 'GET /api/v1/questions' do
-    context 'unauthorized' do
-      it 'returns 401 status if the is no access_token' do
-        get '/api/v1/questions', headers: headers
-
-        expect(response.status).to eq 401
-      end
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/questions', params: { access_token: '1234' }, headers: headers
-
-        expect(response.status).to eq 401
-      end
+    let(:api_path) { '/api/v1/questions' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
     end
 
     context 'authorized' do
