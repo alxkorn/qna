fdc3ad1f7cb31474b43014ad2983dbfbaad530d0
alxkorn <akorneyev10@yandex.ru>
2019-12-24 20:55:38 +0300

    Added ReputationJob

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -47,4 +47,13 @@ RSpec.describe Question, type: :model do
       end
     end
   end
+
+  describe 'reputation' do
+    let(:question) { build(:question) }
+
+    it 'calls ReputationJob' do
+      expect(ReputationJob).to receive(:perform_later).with(question)
+      question.save!
+    end
+  end
 end
