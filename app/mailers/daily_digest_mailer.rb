06a3e8ba984982803e931c0aa82edd6e8564d9d8
alxkorn <akorneyev10@yandex.ru>
2019-12-25 13:29:10 +0300

    DailyDigest view

--- app/mailers/daily_digest_mailer.rb
+++ app/mailers/daily_digest_mailer.rb
@@ -7,6 +7,8 @@ class DailyDigestMailer < ApplicationMailer
   #
   def digest(user)
     @greeting = "Hi"
+    now = Time.zone.now
+    @questions = Question.where(created_at: (now - 24.hours)..now)
 
     mail to: user.email
   end
